e8ba2491d3788a7a292124597a679335757f7c78
fenatan <felipenatan@novaandradina.org>
2020-02-10 16:10:53 -0400

    Adding Unit test

---
+++ test/unit/controllers/products_spec.js
@@ -0,0 +1,25 @@
+import ProductsController from '../../../src/controllers/products';
+import sinon from 'sinon';
+
+describe('Controllers: Products', () => {
+    const defaultProduct = [{
+        name: 'Default Product',
+        description: 'product description',
+        price: 100
+    }];
+
+    describe('get() products', () => {
+        it('should return a list of products', () => {
+            const request = {};
+            const response = {
+                send: sinon.spy()
+            };
+
+            const productsController = new ProductsController();
+            productsController.get(request, response);
+            
+            expect(response.send.called).to.be.true;
+            expect(response.send.calledWith(defaultProduct)).to.be.true;
+        });
+    });
+});
